ddbe55db6d6a78cefaf0a086d4a318a7654777d3
krithikV <45972552+krithikV@users.noreply.github.com>
2020-06-20 08:29:55 +0530

    Add files via upload

---
+++ opencv_onestep.py
@@ -0,0 +1,2 @@
+import os
+os.system('sudo apt-get update; sudo apt-get upgrade; sudo apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4; sudo pip3 install opencv-contrib-python libwebp6; pip3 install python-numpy; pip3 install python-matplotlib')
